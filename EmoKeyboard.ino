2f072d74e207f0059a572ad114018b8715f4419b
Takuro Tsujikawa <takuro.tsujikawa@gmail.com>
2022-10-22 10:56:43 +0900

    clean up code

--- EmoKeyboard.ino
+++ EmoKeyboard.ino
@@ -1,6 +1,3 @@
-/**
- * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
- */
 #include <BleKeyboard.h>
 
 BleKeyboard bleKeyboard("EMO Keyboard");
@@ -19,29 +16,17 @@ void setup() {
 void loop() {
   if(bleKeyboard.isConnected()) {
     curSwitchState = digitalRead(19);
-//    Serial.print(curSwitchState);
-//    Serial.print(" ");
-//    Serial.print(prevSwitchState);
     if(prevSwitchState == HIGH && curSwitchState == LOW){
-//      Serial.println("Sending 'Hello world'...");
+      // git push
       bleKeyboard.print("git push");
-  
       delay(100);
-  
-//      Serial.println("Sending Enter key...");
       bleKeyboard.write(KEY_RETURN);
-  
-     //
-     // Below is an example of pressing multiple keyboard modifiers 
-     // which by default is commented out.
-      /*
-      Serial.println("Sending Ctrl+Alt+Delete...");
-      bleKeyboard.press(KEY_LEFT_CTRL);
-      bleKeyboard.press(KEY_LEFT_ALT);
-      bleKeyboard.press(KEY_DELETE);
-      delay(100);
-      bleKeyboard.releaseAll();
-      */
+
+      // ENG - JPN language change
+//      bleKeyboard.press(KEY_LEFT_CTRL);
+//      bleKeyboard.print(" ");
+//      delay(100);
+//      bleKeyboard.releaseAll();
     }
     prevSwitchState = curSwitchState;
   }
